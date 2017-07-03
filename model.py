8f936c5bf00199b9274394417499b18f8d3628ad
Sruc73 <michaelmares@outlook.com>
2017-07-03 18:29:16 +0200

    Added all files

---
+++ model.py
@@ -0,0 +1,47 @@
+import json
+import math
+
+class Agent():
+    def __init__(self, position, **agent_attributes):
+        self.position = position
+        for attr_name, attr_value in agent_attributes.items():
+            setattr(self, attr_name, attr_value)
+
+class Position:
+    def __init__(self, longitude_degrees, latitude_degrees):
+        self.latitude_degrees = latitude_degrees
+        self.longitude_degrees = longitude_degrees
+
+    @property
+    def longitude(self):
+        return self.longitude_degrees * math.pi / 180
+
+    @property
+    def latitude(self):
+        return self.latitude_degrees * math.pi / 180
+
+class Zone:
+    MIN_LONGITUDE_DEGREES = - 180
+    MAX_LONGITUDE_DEGREES = 180
+    WIDTH_DEGREES = 1 # Degrees of longitude
+
+    def initialize_zones(self):
+        for longitude in range(self.MIN_LONGITUDE_DEGREES, self.MAX_LONGITUDE_DEGREES, self.WIDTH_DEGREES):
+            # zone = Zone(bottom_left_corner, top_right_corner)
+
+    def  __init__(self, corner1, corner2):
+        self.corner1 = corner1
+        self.corner2 = corner2
+        self.inhabitants = 0
+
+
+def main():
+    for agent_attributes in json.load(open("agents-100k.json")):
+        latitude = agent_attributes.pop("latitude")
+        longitude = agent_attributes.pop("longitude")
+        position = Position(longitude, latitude)
+        agent = Agent(position, **agent_attributes)
+
+        print(agent.position.longitude)
+
+main()
