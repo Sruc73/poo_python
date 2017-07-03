fb34ae58f2b65a8e82eccf0681784161e264b8c6
Sruc73 <michaelmares@outlook.com>
2017-07-04 00:04:40 +0200

    Add a loop for latitude and created the atribut ZONES

--- model.py
+++ model.py
@@ -24,15 +24,17 @@ class Zone:
     MIN_LONGITUDE_DEGREES = - 180
     MAX_LONGITUDE_DEGREES = 180
     MIN_LATITUDE_DEGREES = -90
-    MAS_LATITUDE_DEGREES = 90
+    MAX_LATITUDE_DEGREES = 90
     WIDTH_DEGREES = 1 # Degrees of longitude
-    HEIFHT_DEGREES = 1 # Degrees of latitude
+    HEIGHT_DEGREES = 1 # Degrees of latitude
+    ZONES = []
 
     def initialize_zones(self):
-        for longitude in range(self.MIN_LONGITUDE_DEGREES, self.MAX_LONGITUDE_DEGREES, self.HEIGHT_DEGREES):
-            bottom_left_corner = Position(longitude, 1)
-            top_right_corner = Position(longitude + self.WIDTH_DEGREES, 1 + self.WIDTH_DEGREES)
-            zone = Zone(bottom_left_corner, top_right_corner)
+        for latitude in range(self.MIN_LATITUDE_DEGREES, self.MAX_LATITUDE_DEGREES, self.HEIGHT_DEGREES):
+            for longitude in range(self.MIN_LONGITUDE_DEGREES, self.MAX_LONGITUDE_DEGREES, self.WIDTH_DEGREES):
+                bottom_left_corner = Position(longitude, 1)
+                top_right_corner = Position(longitude + self.WIDTH_DEGREES, 1 + self.WIDTH_DEGREES)
+                zone = Zone(bottom_left_corner, top_right_corner)
 
     def  __init__(self, corner1, corner2):
         self.corner1 = corner1
