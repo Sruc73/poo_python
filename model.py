381296625845960beca7839fc09663bd12497b33
Sruc73 <michaelmares@outlook.com>
2017-07-04 12:57:39 +0200

    Graph completed

--- model.py
+++ model.py
@@ -104,13 +104,29 @@ class Zone:
         return sum([inhabitant.agreeableness for inhabitant in self.inhabitants]) / self.population
 
 class BaseGraph:
-
     def __init__(self):
         self.title = "Your graph title"
-        self.x_label = "X-axis lable"
+        self.x_label = "X-axis label"
         self.y_label = "Y-axis label"
         self.show_grid = True
 
+    def xy_values(self, zones):
+        raise NotImplementedError
+
+class agreeableness(BaseGraph):
+    def __init__(self):
+        super().def __init__() # Execute the parent's __init__() method
+        self.title = "Nice people live in the countryside"
+        self.x_label = "population density"
+        self.y_label = "agreeableness"
+
+    def xy_values(self, zones):
+        x_values = [zone.population_density() for zone in zones]
+        y_values = [zone.average_agreeableness() for zone in zones]
+        return x_values, y_values
+
+
+class Graph:
     def show(self, zones):
         # x_values = gather only x_values from zones
         # y_values = gather only y_values from zones
